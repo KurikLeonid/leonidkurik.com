397426fcdb51199a44cd2d5753965713ba570272
KurikLeonid <47047963+KurikLeonid@users.noreply.github.com>
2020-03-31 12:21:48 +1000

    1
    1

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,56 @@
 $(function() {
 		$('.work-preview__container').slick({
 			arrows: false,
-			infinite: true
+			infinite: true,
+			speed: 500,
+			fade: true,
+			cssEase: 'linear',
+			verticalSwiping: true
 		});
+		$('.work-preview__container').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+			$('.work-name').slick('slickNext');
+			$('.work-info__amount').slick('slickNext');
+			$('.work-type').slick('slickNext');
+			$('.slider__pagination_mouse').slick('slickNext');
+		});		
+});
+
+$('.work-name').slick({
+	arrows: false,
+	infinite: true,
+	speed: 100,
+	fade: true,
+	verticalSwiping: true
+});
+
+$('.work-info__amount').slick({
+	arrows: false,
+	infinite: true,
+	speed: 100,
+	fade: true,
+	verticalSwiping: true
+});
+
+$('.work-type').slick({
+	arrows: false,
+	infinite: true,
+	speed: 100,
+	fade: true,
+	verticalSwiping: true
+});
+
+$(function() {
+	$('.slider__pagination_mouse').slick({
+		arrows: false,
+		infinite: true,
+		speed: 100,
+		fade: true,
+		verticalSwiping: true
+		});	
+		$('.slider__pagination_mouse').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+			$('.work-name').slick('slickNext');
+			$('.work-info__amount').slick('slickNext');
+			$('.work-type').slick('slickNext');
+			$('.work-preview__container').slick('slickNext');
+		});		
 });
